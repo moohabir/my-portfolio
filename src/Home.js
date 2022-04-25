43cb84fe092cb60f99cc8b0282b07acf4ce27719
moohabir <87937848+moohabir@users.noreply.github.com>
2022-04-25 03:08:00 +0200

    Home Component was made to have other components init

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import About from "./About";
+import Projects from "./Projects";
+import Contacts from "./Contacts";
+import Login from "./Login";
 
 export default function Home() {
   return (
     <div>
       <h1>Home Component</h1>
+      <About />
+      <Projects />
+      <Contacts />
+      <Login />
     </div>
   );
 }
