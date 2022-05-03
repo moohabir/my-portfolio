f831730f726109a4101c09d14fafebfcecd32d6b
moohabir <87937848+moohabir@users.noreply.github.com>
2022-05-04 01:37:41 +0200

    edited little in About page and css but need further

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import heroImage from "../assets/heroImage.png";
 export default function About() {
   return (
     <div>
-      <h1>About Component</h1>
+      {/*image will be left side on mobile column above and center*/}
+      <div className="hero-left">
+        <img src={heroImage} alt="" className="hero-image" />
+      </div>
+
+      {/*below will be right side*/}
+      <div className="hero-right">
+        <h1 className="hero-title">
+          I am Front-end <br /> Web Developer
+        </h1>
+        <h1 className="hero-title">And Blockchain Developer</h1>
+        <button className="hero-p"> Hire Me </button>
+        <button className="hero-p"> Contact Me </button>
+      </div>
     </div>
   );
 }
