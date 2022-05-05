89b3a18a70cef7db4bf525112278c5e245ad894c
moohabir <87937848+moohabir@users.noreply.github.com>
2022-05-06 00:58:55 +0200

    contact form styled little bit

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -3,27 +3,25 @@ import React from "react";
 export default function Contacts() {
   return (
     <form className="form">
+      <h1>Contact Me</h1>
       <input
         type="text"
         placeholder="Your Name"
         name="name"
-        value=""
         className="input"
       />
       <input
         type="email"
         placeholder="Your Email"
         name="email"
-        value=""
         className="email"
       />
       <textarea
         placeholder="Write Massage"
         name="comments"
-        value=""
         className="comments"
       />
-      <button>Send message</button>
+      <button className="form-btn">Send message</button>
     </form>
   );
 }
