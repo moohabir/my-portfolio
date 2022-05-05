8b5ed1129751dca2c2d52f7010488fbc29d208fd
moohabir <87937848+moohabir@users.noreply.github.com>
2022-05-06 00:31:19 +0200

    contacted added with  Form

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -2,10 +2,27 @@ import React from "react";
 
 export default function Contacts() {
   return (
-    <form>
-      <input type="text" placeholder="Your Name" name="name" value="" />
-      <input type="email" placeholder="Your Email" name="email" value="" />
-      <input type="text" placeholder="Write Massage" name="msg" value="" />
+    <form className="form">
+      <input
+        type="text"
+        placeholder="Your Name"
+        name="name"
+        value=""
+        className="input"
+      />
+      <input
+        type="email"
+        placeholder="Your Email"
+        name="email"
+        value=""
+        className="email"
+      />
+      <textarea
+        placeholder="Write Massage"
+        name="comments"
+        value=""
+        className="comments"
+      />
       <button>Send message</button>
     </form>
   );
